13039d2fd8b032e367e8d1bf39bae0c1a797bd38
Nelson Darkwah Oppong <n@darkwahoppong.com>
2017-09-29 20:23:10 +0200

    Remove setuptools-scm and use hard coded version

--- plx_gpib_ethernet/__init__.py
+++ plx_gpib_ethernet/__init__.py
@@ -1,7 +1,9 @@
 from .plx_gpib_ethernet import PrologixGPIBEthernet
 from .plx_gpib_ethernet_device import PrologixGPIBEthernetDevice
+from .version import __version__
 
 __all__ = ['plx_gpib_ethernet',
            'plx_gpib_ethernet_device',
            'PrologixGPIBEthernet',
-           'PrologixGPIBEthernetDevice']
+           'PrologixGPIBEthernetDevice',
+           '__version__']
