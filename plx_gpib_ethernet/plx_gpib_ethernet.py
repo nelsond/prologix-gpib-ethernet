308926c213d20bb4445e6225494ff3edb54db09e
Nelson Darkwah Oppong <n@darkwahoppong.com>
2020-12-05 16:34:11 +0100

    Add PrologixGPIBEthernet.query_controller

--- plx_gpib_ethernet/plx_gpib_ethernet.py
+++ plx_gpib_ethernet/plx_gpib_ethernet.py
@@ -33,13 +33,16 @@ class PrologixGPIBEthernet:
         self._send(cmd)
 
     def read(self, num_bytes=1024):
+        self._send('++read eoi')
         return self._recv(num_bytes)
 
     def query(self, cmd, buffer_size=1024*1024):
         self.write(cmd)
-        if(cmd.startswith('++') == False):
-            self._send('++read eoi')
         return self.read(buffer_size)
+
+    def query_controller(self, cmd, buffer_size=1024*1024):
+        self._send(cmd)
+        return self._recv(buffer_size)
 
     def _send(self, value):
         encoded_value = ('%s\n' % value).encode('ascii')
